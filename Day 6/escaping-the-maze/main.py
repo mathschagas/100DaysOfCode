00b3853b45898df54f6451ce96efd928d42a5b0d
Matheus Chagas <matheus.chagas@aluno.uece.br>
2023-07-29 09:42:33 -0300

    Days 4, 5 and 6

---
+++ Day 6/escaping-the-maze/main.py
@@ -0,0 +1,35 @@
+# THIS CODE ONLY WORKS ON THE REEBORG'S WORLD ENVIRONMENT
+
+# def turn_around():
+#     turn_left()
+#     turn_left()
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def jump():
+#     turn_left()
+#     while wall_on_right():
+#         move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     while not wall_in_front():
+#         move()
+#     turn_left()
+
+# while front_is_clear():
+#     move()
+
+# turn_left()
+
+# while not at_goal():
+#     if front_is_clear() and wall_on_right():
+#         move()
+#     elif not wall_on_right():
+#         turn_right()
+#         move()
+#     else:
+#         turn_left()
