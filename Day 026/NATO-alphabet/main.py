0a83025e80cedef163e4a377f3cb2aba3ea57605
Matheus Chagas <matheus.chagas@aluno.uece.br>
2023-08-27 18:49:14 -0300

    Day 26 - Dicts and Lists Comprehensions

---
+++ Day 026/NATO-alphabet/main.py
@@ -0,0 +1,26 @@
+#Looping through dictionaries:
+# for (key, value) in student_dict.items():
+#     #Access key and value
+#     pass
+
+
+#Loop through rows of a data frame
+# for (index, row) in student_data_frame.iterrows():
+    #Access index and row
+    #Access row.student or row.score
+    # pass
+
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+import pandas
+
+#TODO 1. Create a dictionary in this format:
+# {"A": "Alfa", "B": "Bravo"}
+nato_df = pandas.read_csv("Day 026/NATO-alphabet/nato_phonetic_alphabet.csv")
+nato_dict = {row["letter"]:row["code"] for (index, row) in nato_df.iterrows()}
+
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+word = input("Enter a word: ").upper()
+output_list = [nato_dict[letter] for letter in word]
+print(output_list)
